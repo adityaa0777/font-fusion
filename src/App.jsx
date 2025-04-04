a206321f4536fa6f1f8098589d44a6cee387dbb8
adityaa0777 <maskeaditya2003@gmail.com>
2025-04-04 13:04:15 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -109,15 +109,14 @@ return(
                   <div id="bg-images">
                     <h2 id ='sub-heading'>Choose your background Image</h2>
                     <div id="images">
-                      <button  id="bg-img1"onClick={()=>setBackground('sky.jpg')}/>
+                      <button  id="bg-img1"onClick={()=>setBackground('/images/sky.jpg')}/>
+                      <button  id="bg-img2"onClick={()=>setBackground('/images/beach.webp')}/>
 
-                      <button  id="bg-img2"onClick={()=>setBackground('beach.webp')}/>
+                      <button  id="bg-img3"onClick={()=>setBackground('/images/blueflower.jpg')}/>
 
-                      <button  id="bg-img3"onClick={()=>setBackground('blueflower.jpg')}/>
+                      <button  id="bg-img4"onClick={()=>setBackground('/images/forest.jpg')}/>
 
-                      <button  id="bg-img4"onClick={()=>setBackground('forest.jpg')}/>
-
-                      <button  id="bg-img6"onClick={()=>setBackground('ocean.jpg')}/>
+                      <button  id="bg-img6"onClick={()=>setBackground('/images/ocean.jpg')}/>
 
                   
                     </div>
